818e78e230fc99ff86b9f0aef8b566f7ce42df7e
@Rinkesh_Epesign375 <rinkesh@epesign.com>
2024-04-12 15:27:29 +0530

    implemented crud in nest-js using mongodb

---
+++ src/book/dto/create-book.dto.ts
@@ -0,0 +1,11 @@
+import { Category } from "../schemas/book.schema"
+
+
+export class CreateBookDto {
+    readonly title:string;
+    readonly description:string;
+    readonly price:number;
+    readonly author:string;
+    readonly category:Category;
+
+}
---
+++ src/book/dto/update-book.dto.ts
@@ -0,0 +1,11 @@
+import { Category } from "../schemas/book.schema"
+
+
+export class UpdateBookDto {
+    readonly title:string;
+    readonly description:string;
+    readonly price:number;
+    readonly author:string;
+    readonly category:Category;
+
+}
